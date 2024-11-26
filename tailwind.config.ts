a6a210d230568c1ea2c6c825e91ab721bb9579e7
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2024-11-26 14:06:16 +0200

    tailwind theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,46 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brand: {
+          primary: "#4297F9",
+          primaryDark: "#008AF4",
+          primaryLogo: "#72C6FA",
+          logoDark: "#4DC4FC",
+          redAccent: "#FF7474",
+          purpleAccent: "#EEA7FD",
+          orangeAccent: "#F9AA72",
+          greenAccent: "#3DD9B3",
+          black: "#04050C",
+          dark: "#131524",
+          textGray: "#F2F5F9",
+          textGrayDarker: "#F2F4F8",
+          darkGray: "#A3B2C7",
+          error: "#B80000",
+        },
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
+      },
+      boxShadow: {
+        "drop-1": "0px 10px 30px 0px rgba(66, 71, 97, 0.1)",
+        "drop-2": "0 8px 30px 0 rgba(65, 89, 214, 0.3)",
+        "drop-3": "0 8px 30px 0 rgba(65, 89, 214, 0.1)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
